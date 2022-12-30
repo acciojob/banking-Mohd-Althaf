5041b5748b74426c68f76768de92750793d2aa41
Althaf <mohd.althaf337@gmail.com>
2022-12-30 17:22:28 +0530

    msg2

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -53,4 +53,20 @@ public class BankAccount {
     public double getMinBalance() {
         return minBalance;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -3,13 +3,21 @@ package com.driver;
 import java.util.Arrays;
 
 public class CurrentAccount extends BankAccount{
-    String tradeLicenseId; //consists of Uppercase English characters only
+    private String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         super(name,balance,5000);
          this.tradeLicenseId = tradeLicenseId;
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
+    }
+
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
+
+    public void setTradeLicenseId(String tradeLicenseId) {
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
